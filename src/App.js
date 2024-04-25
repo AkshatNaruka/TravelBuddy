96aa73f5ce2d50adc0d5da2649d2927a2aa1483a
Akshat Naruka <narukaakshat@gmail.com>
2024-04-26 00:56:32 +0530

    share removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css'; // Import CSS file
-import Share from './Share';
+
 
 function App() {
   const [topic, setTopic] = useState('');
